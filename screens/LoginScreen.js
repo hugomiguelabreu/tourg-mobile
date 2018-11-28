f5bdf27d8763b70cf12814711ceb3f252a57c743
Hugo Abreu <hugo.miguel.abreu@gmail.com>
2018-11-28 15:18:53 +0000

    Changed login design

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -36,7 +36,7 @@ export default class LoginScreen extends React.Component {
                         </View>
                         <View style={styles.loginView}>
                             <View style={{flexDirection:'row', justifyContent: 'center'}}>
-                                <Title style={{fontSize:38}}>Login</Title>
+                                <Title style={{fontSize:32}}>Login</Title>
                             </View>
                             <TextInput
                                 label='Email'
@@ -52,9 +52,8 @@ export default class LoginScreen extends React.Component {
                                 style={styles.textInput}
                             />
                             <TouchableOpacity
-                                onPress={this.props.onLoginPress}
                                 style={styles.button}>
-                                <Button mode="contained" onPress={() => console.log('Pressed')}
+                                <Button mode="contained" onPress={() => this.props.navigation.navigate('Main')}
                                         style={styles.buttonLogin}>
                                     Login
                                 </Button>
@@ -84,67 +83,26 @@ const styles = StyleSheet.create({
         backgroundColor:'transparent',
     },
     welcomeContainer: {
-      flex: 1,
-      flexDirection: 'column',
-      marginTop: 10,
-      marginBottom: 20,
-      marginLeft: 45,
-      marginRight: 45,
+        flex: 1,
+        flexDirection: 'column',
+        marginTop: 10,
+        marginBottom: 20,
+        marginLeft: 45,
+        marginRight: 45,
     },
     imageView:{
         flex:1,
         flexDirection:'row',
-        height: 65,
         marginTop:30,
     },
     loginView:{
-        flex:3,
+        flex:4,
         flexDirection:'column',
-        height:100,
     },
     welcomeImage: {
         flex:1,
         height:65,
         flexDirection: 'row',
-        resizeMode: 'cover',
-    },
-    tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-        backgroundColor: '#fbfbfb',
-        paddingVertical: 20,
-    },
-    tabBarInfoText: {
-        fontSize: 17,
-        color: 'rgba(96,100,109, 1)',
-        textAlign: 'center',
-    },
-    navigationFilename: {
-        marginTop: 5,
-    },
-    helpContainer: {
-        marginTop: 15,
-        alignItems: 'center',
-    },
-    helpLink: {
-        paddingVertical: 15,
-    },
-    helpLinkText: {
-        fontSize: 14,
-        color: '#2e78b7',
+        resizeMode: 'contain',
     },
 });
